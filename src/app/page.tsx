1b10b5f52b8b1bfbdeb74939f83ffbb764f47509
Adam del Cano <adam.j.delcano@gmail.com>
2024-03-30 04:55:55 -0400

    added stack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
   const myName = useRef(null);
   const mission = useRef(null);
   const whyCare = useRef(null);
+  const contactPointer = useRef(null);
 
   useEffect(() => {
     const nameTyped = new Typed(myName.current, {
@@ -31,16 +32,21 @@ export default function Home() {
       strings: ["^2000 You want me to do that for you."],
       typeSpeed: 50,
     });
+    const contactTyped = new Typed(contactPointer.current, {
+      strings: ["^4000 Check the bottom of the page for contact information."],
+      typeSpeed: 50,
+    });
 
     return () => {
       // Destroy Typed instance during cleanup to stop animation
       nameTyped.destroy();
       missionTyped.destroy();
       whyCareTyped.destroy();
+      contactTyped.destroy();
     };
   }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-12 px-18 crt">
+    <main className="flex min-h-screen flex-col items-center justify-start p-12 px-18 crt bg-[url('/2630264.jpg')] bg-cover">
       <header className="flex flex-row justify-between w-full text-green-400 font-mono p-18">
         <p className="flex flex-row text-5xl has-tooltip">
           ⍝
@@ -64,116 +70,149 @@ export default function Home() {
           </a>
         </div>
       </header>
-      <form className="w-full max-w-lg self-start justify-self-start">
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
-              htmlFor="grid-first-name"
-            >
-              Who is this clown?
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-900 text-green-400 border border-gray-900 rounded py-3 px-4 mb-3 leading-tight disabled"
-              id="grid-first-name"
-              type="text"
-              ref={myName}
-            ></input>
+      <div className="flex flex-row w-full justify-evenly">
+        <form className="max-w-lg self-start justify-self-start">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
+                htmlFor="grid-name"
+              >
+                Who is this clown?
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-900 text-green-400 border border-gray-900 rounded py-3 px-4 mb-3 leading-tight disabled"
+                id="grid-name"
+                type="text"
+                ref={myName}
+              />
+            </div>
+            <div className="w-full md:w-1/2 px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
+                htmlFor="grid-mission"
+              >
+                What does he do?
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-900 text-green-400 border border-gray-900 rounded py-3 px-4 leading-tight disabled"
+                id="grid-mission"
+                type="text"
+                placeholder=""
+                ref={mission}
+              />
+            </div>
           </div>
-          <div className="w-full md:w-1/2 px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
-              htmlFor="grid-last-name"
-            >
-              What does he do?
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-900 text-green-400 border border-gray-900 rounded py-3 px-4 leading-tight disabled"
-              id="grid-last-name"
-              type="text"
-              placeholder=""
-              ref={mission}
-            ></input>
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
+                htmlFor="grid-password"
+              >
+                Why should I care?
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-900 text-green-400 border border-gray-900 rounded py-3 px-4 leading-tight disabled"
+                id="grid-password"
+                ref={whyCare}
+              />
+            </div>
           </div>
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
+                htmlFor="contact-pointer"
+              >
+                How can I get in touch?
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-900 text-green-400 border border-gray-900 rounded py-3 px-4 leading-tight disabled"
+                id="contact-pointer"
+                ref={contactPointer}
+              />
+            </div>
+          </div>
+        </form>
+        <div
+          id="typed-message"
+          className="flex self-center align-center items-center justify-center mx-auto"
+        >
+          <p className="text-green-300 font-mono rounded py-3 px-4 pr-1 mt-24 text-sm bg-gray-700/75">
+            Surely <span className="text-red-100">thou</span> wert void of
+            greatness,
+            <br />
+            Having neither strength nor beauty,
+            <br />
+            When the moose was trampling on{" "}
+            <span className="text-red-100">thee</span>,
+            <br />
+            When the roebuck trod upon{" "}
+            <span className="text-red-100">thee</span>
+            ,
+            <br />
+            When the tracks of wolves were in{" "}
+            <span className="text-red-100">thee</span>,
+            <br />
+            And the bear-paws scratched{" "}
+            <span className="text-red-200">thy</span> body.
+            <br />
+            Surely <span className="text-red-200">thou</span> hadst little value
+            <br />
+            When the skilful Ilmarinen,
+            <br />
+            First of all the iron-workers,
+            <br />
+            Brought <span className="text-red-200">thee</span> from the
+            blackened swamp-lands,
+            <br />
+            Took <span className="text-red-200">thee</span> to his ancient
+            smithy,
+            <br />
+            Placed <span className="text-red-300">thee</span> in his fiery
+            furnace.
+            <br />
+            Truly <span className="text-red-300">thou</span> hadst little vigor,
+            <br />
+            Little strength, and little danger,
+            <br />
+            When <span className="text-red-300">thou</span> in the fire wert
+            hissing,
+            <br />
+            Rolling forth like seething water,
+            <br />
+            From the furnace of the smithy,
+            <br />
+            When <span className="text-red-300">thou</span> gavest oath the
+            strongest,
+            <br />
+            By the furnace, by the anvil,
+            <br />
+            By the tongs, and by the hammer,
+            <br />
+            By the dwelling of the blacksmith,
+            <br />
+            By the <span className="text-red-500">fire</span> within the{" "}
+            <span className="text-red-600">furnace</span>.
+            <br />
+          </p>
         </div>
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-300 text-xs font-bold mb-2"
-              htmlFor="grid-password"
-            >
-              Why should I care?
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-900 text-green-400 border border-gray-900 rounded py-3 px-4 leading-tight disabled"
-              id="grid-password"
-              ref={whyCare}
-            ></input>
-          </div>
-        </div>
-      </form>
-      <div className="flex flex-row justify-between w-full">
-        <p className="text-green-400 font-mono w-1/3">
-          Surely <span className="text-gray-600">thou</span> wert void of
-          greatness,
+        <p className="max-w-lg text-green-500">
           <br />
-          Having neither strength nor beauty,
+          Familiar Frontend stack:{" "}
+          <span className="text-green-400">
+            TypeScript, React, Next.js, Tailwind CSS
+          </span>
           <br />
-          When the moose was trampling on{" "}
-          <span className="text-gray-600">thee</span>,
+          Familiar Backend stack:{" "}
+          <span className="text-green-400">Python, GraphQL, MongoDB</span>
           <br />
-          When the roebuck trod upon <span className="text-gray-600">thee</span>
-          ,
-          <br />
-          When the tracks of wolves were in{" "}
-          <span className="text-gray-600">thee</span>,
-          <br />
-          And the bear-paws scratched <span className="text-gray-600">
-            thy
-          </span>{" "}
-          body.
-          <br />
-          Surely <span className="text-gray-600">thou</span> hadst little value
-          <br />
-          When the skilful Ilmarinen,
-          <br />
-          First of all the iron-workers,
-          <br />
-          Brought <span className="text-gray-600">thee</span> from the blackened
-          swamp-lands,
-          <br />
-          Took <span className="text-gray-600">thee</span> to his ancient
-          smithy,
-          <br />
-          Placed <span className="text-gray-600">thee</span> in his fiery
-          furnace.
-          <br />
-          Truly <span className="text-gray-600">thou</span> hadst little vigor,
-          <br />
-          Little strength, and little danger,
-          <br />
-          When <span className="text-gray-600">thou</span> in the fire wert
-          hissing,
-          <br />
-          Rolling forth like seething water,
-          <br />
-          From the furnace of the smithy,
-          <br />
-          When <span className="text-gray-600">thou</span> gavest oath the
-          strongest,
-          <br />
-          By the furnace, by the anvil,
-          <br />
-          By the tongs, and by the hammer,
-          <br />
-          By the dwelling of the blacksmith,
-          <br />
-          By the <span className="text-red-400">fire</span> within the{" "}
-          <span className="text-red-400">furnace</span>.
-          <br />
+          Familiar Spirit:{" "}
+          <span className="text-green-400">Peridot the cat</span>
         </p>
+
       </div>
-      <footer className="fixed bottom-0 flex flex-row items-center justify-evenly w-full text-green-400 font-mono text-xs">
+      <footer className="fixed bottom-0 flex flex-row items-center justify-evenly w-full text-green-400 font-mono text-xs bg-gray-900/75">
         <p>
           <span className="text-green-600">Professional Inquiries: </span>
           adam.j.delcano@gmail.com
@@ -186,19 +225,12 @@ export default function Home() {
           <span className="text-green-600">Elsewhere: </span> Wherever
           robotmascot isn&apos;t the UK marketing agency
         </p>
-        <p className="flex flex-row self-center">
-          <span>
-            <Image width={32} height={32} src="/grunt.png" alt="Grunt" />
-          </span>
-          <span className="self-center">2024 Adam del Cano</span>
-          <span>
-            <Image
-              width={32}
-              height={32}
-              src="/grunt-defend-2.png"
-              alt="Grunt"
-            />
-          </span>
+        <p className="flex flex-row self-center">2024 Adam del Cano</p>
+        <p>
+          <a href="https://www.freepik.com/free-vector/retro-futuristic-landscape-background-with-sun_5072281.htm#query=horizon%20grid&position=8&from_view=keyword&track=ais&uuid=e6966af8-f0dd-4c34-adf6-33cab7e6124a">
+            Background Image by pikisuperstar
+          </a>{" "}
+          on Freepik
         </p>
       </footer>
     </main>
