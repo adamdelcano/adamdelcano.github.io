3957a6d2bb4b5181722a0888206032d491b56c57
Adam del Cano <adam.j.delcano@gmail.com>
2024-05-21 19:43:54 -0400

    Two rows for real i hope

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -242,66 +242,8 @@ export default function Home() {
           <div className="cube__face cube__face--bottom">bottom</div>
         </div>
       </div>
-      <div className="scene">
-        <div className="cube">
-          <div className="cube__face cube__face--front">front</div>
-          <div className="cube__face cube__face--back">back</div>
-          <div className="cube__face cube__face--right">right</div>
-          <div className="cube__face cube__face--left">left</div>
-          <div className="cube__face cube__face--top">top</div>
-          <div className="cube__face cube__face--bottom">bottom</div>
-        </div>
       </div>
-      <div className="scene">
-        <div className="cube">
-          <div className="cube__face cube__face--front">front</div>
-          <div className="cube__face cube__face--back">back</div>
-          <div className="cube__face cube__face--right">right</div>
-          <div className="cube__face cube__face--left">left</div>
-          <div className="cube__face cube__face--top">top</div>
-          <div className="cube__face cube__face--bottom">bottom</div>
-        </div>
-      </div>
-      <div className="scene">
-        <div className="cube">
-          <div className="cube__face cube__face--front">front</div>
-          <div className="cube__face cube__face--back">back</div>
-          <div className="cube__face cube__face--right">right</div>
-          <div className="cube__face cube__face--left">left</div>
-          <div className="cube__face cube__face--top">top</div>
-          <div className="cube__face cube__face--bottom">bottom</div>
-        </div>
-      </div>
-      <div className="scene">
-        <div className="cube">
-          <div className="cube__face cube__face--front">front</div>
-          <div className="cube__face cube__face--back">back</div>
-          <div className="cube__face cube__face--right">right</div>
-          <div className="cube__face cube__face--left">left</div>
-          <div className="cube__face cube__face--top">top</div>
-          <div className="cube__face cube__face--bottom">bottom</div>
-        </div>
-      </div>
-      <div className="scene">
-        <div className="cube">
-          <div className="cube__face cube__face--front">front</div>
-          <div className="cube__face cube__face--back">back</div>
-          <div className="cube__face cube__face--right">right</div>
-          <div className="cube__face cube__face--left">left</div>
-          <div className="cube__face cube__face--top">top</div>
-          <div className="cube__face cube__face--bottom">bottom</div>
-        </div>
-      </div>
-      <div className="scene">
-        <div className="cube">
-          <div className="cube__face cube__face--front">front</div>
-          <div className="cube__face cube__face--back">back</div>
-          <div className="cube__face cube__face--right">right</div>
-          <div className="cube__face cube__face--left">left</div>
-          <div className="cube__face cube__face--top">top</div>
-          <div className="cube__face cube__face--bottom">bottom</div>
-        </div>
-      </div>
+      <div className="flex flex-row w-full justify-evenly">
       <div className="scene">
         <div className="cube">
           <div className="cube__face cube__face--front">front</div>
