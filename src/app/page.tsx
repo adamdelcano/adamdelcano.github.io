e443b098432d681d2486fdeaf2ac91ebef188460
Adam del Cano <adam.j.delcano@gmail.com>
2024-05-21 19:50:47 -0400

    Change justification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
           </a>
         </div>
       </header>
-      <div className="flex flex-row w-full justify-evenly">
+      <div className="flex flex-row w-full justify-between">
         <form className="max-w-lg self-start justify-self-start">
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
