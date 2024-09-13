d3dd7db96361f991110597de490a8abe0db33672
Adam del Cano <adam.j.delcano@gmail.com>
2024-09-13 15:03:36 -0400

    hedron test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -151,7 +151,7 @@ export default function Home() {
       <div className="flex flex-row w-full justify-evenly my-20 py-5">
       <div className="border-4 border-slate-600 before:block before:absolute before:-inset-1 before:-skew-y-6 before:bg-pink-500/50 before:hover:bg-pink-500/75 before:scale-50 before:hover:scale-75 relative inline-block">
       <div className="solid">
-        <div className="side"></div>
+        <div className="side"><p>Project A</p></div>
         <div className="side"></div>
         <div className="side"></div>
         <div className="side"></div>
