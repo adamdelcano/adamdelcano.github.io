f75134d9538786447f22a99cd733d31cf2f47a65
Adam del Cano <adam.j.delcano@gmail.com>
2024-09-28 14:28:05 -0400

    Fixed header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 
@@ -6,7 +7,9 @@ export default function Header() {
     <>
       <header className="flex flex-col md:flex-row justify-items-end w-full text-green-200 font-mono p-18">
         <div className="text-5xl text-center md:text-left has-tooltip w-screen max-w-full">
-          <Link href="/" className="no-underline">⍲</Link>
+          <Link href="/" className="no-underline">
+            ⍲
+          </Link>
           <span className="tooltip m-18 max-w-full">
             {"<-"}If you&apos;re curious, this is the{" "}
             <a
@@ -19,16 +22,28 @@ export default function Header() {
           </span>
         </div>
         <div className="flex max-sm:flex-wrap md:text-xl justify-items-end md:gap-6">
-          <Link href="/about" className="m-auto transition-all ease-in-out duration-300 hover:bg-green-900 no-underline">
+          <Link
+            href="/about"
+            className="m-auto transition-all ease-in-out duration-300 hover:bg-green-900 no-underline"
+          >
             ABOUT
           </Link>
-          <Link href="#" className="m-auto transition-all ease-in-out duration-300 hover:bg-green-900 no-underline">
+          <Link
+            href="#"
+            className="m-auto transition-all ease-in-out duration-300 hover:bg-green-900 no-underline"
+          >
             WRITING
           </Link>
-          <Link href="#" className="m-auto transition-all ease-in-out duration-300 hover:bg-green-900 no-underline">
+          <Link
+            href="#"
+            className="m-auto transition-all ease-in-out duration-300 hover:bg-green-900 no-underline"
+          >
             PROJECTS
           </Link>
-          <Link href="#" className="m-auto transition-all ease-in-out duration-300 hover:bg-green-900 no-underline">
+          <Link
+            href="#"
+            className="m-auto transition-all ease-in-out duration-300 hover:bg-green-900 no-underline"
+          >
             RESUME
           </Link>
         </div>
